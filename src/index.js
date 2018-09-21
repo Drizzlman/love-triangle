b8d105393419ecc40b606aca8c78fb71095facf9
Kovalko Alexey <kovalko.alexey@yandex.by>
2018-09-21 17:18:33 +0300

    Task_2

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  var number = 0;
-   for (count = 0;count<preferences.length; count++){
-   if(preferences[preferences[preferences[count]-1]-1]-1 === count )
+   var number = 0;
+   count = 0;
+   for (;count<preferences.length; count++){
+   if(preferences[preferences[preferences[count]-1]-1]-1 == count )
     number++;
    }
    return parseInt(number/3);
+
 };
